2782aed78baebe37b1c2764489a77cfd5ebf7d49
急急如律令 <kyukyunyorituryo@live.jp>
2023-09-04 00:44:10 +0900

    アマゾンリンクカード

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,51 @@
-// src/pages/index.js
+// src/pages/page.js
 import React from "react"
 import { Link } from "gatsby"
-import Header from "../components/header"
-export default () => (
-  <div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
-    <Header headerText="Hello Gatsby!" />
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
-)
+import "../styles/default.css"
+import { graphql, useStaticQuery } from "gatsby"
+
+export default function Page() {
+  const data = useStaticQuery(graphql`
+query MyQuery {
+  allJson {
+    edges {
+      node {
+        Asin
+        Booktype
+        Category
+        Contributor
+        ImageURL
+        Points
+        Price
+        Publisher
+        Title
+        URL
+      }
+    }
+  }
+}
+  `)
+  const book = data.allJson.edges
+const asin=["B08XWXXS7W","B08HSVRZ8P"]
+
+ var file = book.filter(word => asin.includes(word.node.Asin));
+console.log(book)
+    return (
+    <div>
+      {file.map(e => (
+      <div>
+        <a className="amazon-card-container" href={e.node.URL}>
+          <div className="amazon-card-body">
+            <div className="amazon-card-title">{e.node.Title}</div>
+            <div className="amazon-card-domain">{e.node.Contributor}、出版社：{e.node.Publisher}</div>
+          </div>
+          <div className="amazon-card-image-container">
+            <img className="amazon-card-image" src={e.node.ImageURL} loading="lazy" alt={e.node.Title}/>
+          </div>
+        </a>
+      </div>
+
+      ))}
+    </div>
+    )
+}
